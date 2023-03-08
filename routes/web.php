b96627afbe8c8a3953e16f1db3c0eb04d966a4f8
zhaojingning <784955168@qq.com>
2023-03-08 16:48:17 +0800

    生成用户认证代码

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,40 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', 'PagesController@root')->name('root');
+
+//Auth::routes();  // 为了更加直观，我们将在 web.php 中使用以下替换 Auth::routes();。
+
+/* Auth::routes()替换 start*/
+
+// 用户身份验证相关的路由
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// 用户注册相关路由
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
+
+// 密码重置相关路由
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+
+// 再次确认密码（重要操作前提示）
+Route::get('password/confirm', 'Auth\ConfirmPasswordController@showConfirmForm')->name('password.confirm');
+Route::post('password/confirm', 'Auth\ConfirmPasswordController@confirm');
+
+// Email 认证相关路由
+Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
+Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
+Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+
+/* Auth::routes()替换 end*/
+
+
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// 上面这句是自动生成的，将其修改如下，统一调用风格：
+//Route::get('/home', 'HomeController@index')->name('home');
+
+
