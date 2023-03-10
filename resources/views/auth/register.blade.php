5f6aa927cd129cb2e07940dfb1fafeeb5d5d3b07
zhaojingning <784955168@qq.com>
2023-03-10 14:42:05 +0800

    注册验证码

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,6 +61,20 @@
                             </div>
                         </div>
 
+                        <div class="mb-3 row">
+                            <label for="captcha" class="col-md-4 col-form-label text-md-end"> 验证码</label>
+                            <div class="col-md-6">
+                                <input class="form-control<?<?php isset($error) && $error->has('captcha') ? 'is-invalid' : '' ?>" name="captcha" required>
+                                <img src="{{captcha_src('flat')}}" class="thumbnail captcha mt-3 mb-2" onclick="this.src='/captcha/flat?'+Math.random()" title="点击图片重新获取验证码">
+                                @if($errors->has('captcha'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{$errors->first('captcha')}}</strong>
+                                    </span>
+                                @endif
+
+                            </div>
+                        </div>
+
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
