6c34fe004d9d7658a165187747c3ed4f7aa77d33
zhaojingning <784955168@qq.com>
2023-03-13 16:51:54 +0800

    认证后的消息提醒

---
+++ app/Listeners/EmailVerified.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Verified;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class EmailVerified
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  \Illuminate\Auth\Events\Verified  $event
+     * @return void
+     */
+    public function handle(Verified $event)
+    {
+        // 会华丽闪存认证成功后的提醒消息
+        session()->flash('success', '邮箱认证成功^_^');
+    }
+}
