4b35ca56cbddb6fe2fa8389b0d0759db1a857df0
zhaojingning <784955168@qq.com>
2023-03-13 14:57:40 +0800

    强制用户认证

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -9,10 +9,10 @@ class PagesController extends Controller
     //新增root()方法
     public function root()
     {
-        session()->flash('success', 'This is a success alert—check it out!');
-        session()->flash('danger', 'This is a danger alert—check it out!');
-        session()->flash('warning', 'This is a warning alert—check it out!');
-        session()->flash('info', 'This is a info alert—check it out!');
+        // session()->flash('success', 'This is a success alert—check it out!');
+        // session()->flash('danger', 'This is a danger alert—check it out!');
+        // session()->flash('warning', 'This is a warning alert—check it out!');
+        // session()->flash('info', 'This is a info alert—check it out!');
         return view('pages.root');
     }
 }
