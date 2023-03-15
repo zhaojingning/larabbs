fb06c8978ca50456272b5928f15a7d797e322d3d
zhaojingning <784955168@qq.com>
2023-03-15 11:08:54 +0800

    话题排序

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -8,10 +8,13 @@ use App\Models\Category;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category)
+    public function show(Category $category, Request $request, Topic $topic)
     {
         // 读取分类ID关联的话题，并按每页20条分页
-        $topics = Topic::where('category_id', $category->id)->paginate(20);
+        $topics = $topic->withOrder($request->order)
+                        ->where('category_id', $category->id)
+                        ->with('user', 'category') // 预加载 防止n+1问题
+                        ->paginate(20);
 
         // 传参变量话题和分类到模版中
         return view('topics.index', compact('topics', 'category'));
