828b1ac0f0db412cc7de38ce8d0a7396c0d7f5c6
zhaojingning <784955168@qq.com>
2023-03-16 10:13:18 +0800

    用户可以删除自己发的话题

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,4 +50,9 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return $this->hasMany(Topic::class);
     }
+
+    public function isAuthorOf($model)
+    {
+        return $this->id == $model->user_id;
+    }
 }
--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -48,9 +48,14 @@
             <a href="{{ route('topics.edit', $topic->id) }}" class="btn btn-outline-secondary btn-sm" role="button">
               <i class="far fa-edit"></i> 编辑
             </a>
-            <a href="#" class="btn btn-outline-secondary btn-sm" role="button">
-              <i class="far fa-trash-alt"></i> 删除
-            </a>
+            <form action="{{ route('topics.destroy', $topic->id) }}" method="POST" style="display: inline-block;"
+                onsubmit="return confirm('您确定要删除吗？');">
+                {{ csrf_field() }}
+                {{ method_field('DELETE') }}
+                <button type="submit" class="btn btn-outline-secondary btn-sm">
+                  <i class="far fa-trash-alt"></i> 删除
+                </button>
+            </form>
           </div>
 
         </div>
