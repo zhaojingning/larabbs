a02357bec5aaae5b3a4e877de57ac3babd07ad25
zhaojingning <784955168@qq.com>
2023-03-17 19:39:45 +0800

    消息通知有新的回复

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,9 +9,29 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 
+use Illuminate\Support\Facades\Auth;
+
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasApiTokens, HasFactory, Notifiable,MustVerifyEmailTrait;
+    use HasApiTokens, HasFactory, MustVerifyEmailTrait;
+
+    use Notifiable {
+        notify as protected laravelNotify;
+    }
+    public function notify($instance)
+    {
+        // 如果要通知的人是当前用户,且不是在验证邮箱，就不必通知了！
+        if ($this->id == Auth::id()&&get_class($instance)!="Illuminate\Auth\Notifications\VerifyEmail") {
+            return;
+        }
+
+        // 只有数据库类型通知才需提醒，直接发送 Email 或者其他的都 Pass
+        if (method_exists($instance, 'toDatabase')) {
+            $this->increment('notification_count');
+        }
+
+        $this->laravelNotify($instance);
+    }
 
     /**
      * The attributes that are mass assignable.
--- app/Observers/RelyObserver.php
+++ app/Observers/RelyObserver.php
@@ -3,12 +3,22 @@
 namespace App\Observers;
 
 use App\Models\Rely;
+use App\Notifications\TopicReplied;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
 class RelyObserver
 {
+    public function created(Reply $reply)
+    {
+        $reply->topic->reply_count = $reply->topic->replies->count();
+        $reply->topic->save();
+
+        // 通知话题作者有新的评论
+        $reply->topic->user->notify(new TopicReplied($reply));
+    }
+
     public function creating(Rely $rely)
     {
         //
