efc0cf2537c74aa3c7f2e551bebc2330cff3ac97
zhaojingning <784955168@qq.com>
2023-03-19 11:15:59 +0800

    消息通知bug修复提交

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -5,21 +5,20 @@ namespace App\Observers;
 use App\Models\Reply;
 use App\Notifications\TopicReplied;
 
-// creating, created, updating, updated, saving,
-// saved,  deleting, deleted, restoring, restored
-
 class ReplyObserver
 {
+    public function created(Reply $reply)
+    {
+        $reply->topic->reply_count = $reply->topic->replies->count();
+        $reply->topic->save();
+
+        // 通知话题作者有新的评论
+        $reply->topic->user->notify(new TopicReplied($reply));
+    }
+
     public function creating(Reply $reply)
     {
         $reply->content = clean($reply->content, 'user_topic_body');
-    }
-
-    public function created(Reply $reply)
-    {
-        // $reply->topic()->increment('reply_count', 1);
-        $reply->topic->reply_count = $reply->topic->replies->count();
-        $reply->topic->save();
     }
 
     public function updating(Reply $reply)
